ac51764d62df4969d3ceff8c1c89a9f0fb240494
Chris Stevens <chris27stevens@gmail.com>
2016-01-24 16:37:56 +0000

    [Incomplete] Animate score change in-game.

--- view/js/in-game.js
+++ view/js/in-game.js
@@ -1,8 +1,27 @@
-var $score = {"blue": $("#blue-score"), "red": $("#red-score")};
+var $score = {"blue": [$("#blue-score"), $("#blue-score-text")], "red": [$("#red-score"), $("#red-score-text")]};
 
 
 var teamReplicant = nodecg.Replicant("teams")
     .on("change", function(oldVal, newVal) { // On change...
-        $score["blue"].text(newVal["blue"]["team"]["score"] + " - " + newVal["blue"]["team"]["tag"]);
-        $score["red"].text(newVal["red"]["team"]["tag"] + " - " + newVal["red"]["team"]["score"]);
+        var blueScore = newVal["blue"]["team"]["score"] + " - " + newVal["blue"]["team"]["tag"];
+        var redScore = newVal["red"]["team"]["tag"] + " - " + newVal["red"]["team"]["score"];
+        var updateScore = function(team, newScore) {
+            var scoreTimeline = new TimelineLite();
+            scoreTimeline
+                .to($score[team][0], 0.5, {
+                    top: "-110%"
+                })
+                .call(function() {
+                    $score[team][1].text(newScore)
+                })
+                .to($score[team][0], 0.5, {
+                    top: "0%"
+                });
+        };
+        if ($score["blue"][1].text() != blueScore) {
+            updateScore("blue", blueScore)
+        }
+        if ($score["red"][1].text() != redScore) {
+            updateScore("red", redScore)
+        }
     });
