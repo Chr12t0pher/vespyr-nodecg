0b662b011cfdc0d4e1ca7dfa403562efb38ae67b
Chris Stevens <chris27stevens@gmail.com>
2016-02-15 21:30:48 +0000

    Wrap in .on("load", ...) to support preloading scenes.

--- graphics/js/in-game.js
+++ graphics/js/in-game.js
@@ -1,74 +1,81 @@
-var $score = {"blue": [$("#blue-score"), $("#blue-score-text")], "red": [$("#red-score"), $("#red-score-text")]};
-var $inGameTexts = {"left": {"top": $("#bottom-center-left-text-upper"), "bottom": $("#bottom-center-left-text-lower")}, "right": $("#bottom-center-right-text")};
+$(window).on("load", function() {
+    nodecg.sendMessage("loaded", "in-game");
 
-var updateScore = function() {
-    if(!comingUpReplicant.value) { // If the coming up replicant hasn't loaded yet, return.
-        return
-    }
-    var blueScore = teamReplicant.value[comingUpReplicant.value["next_game"]]["blue"]["info"]["score"] + " - " + teamReplicant.value[comingUpReplicant.value["next_game"]]["blue"]["info"]["tag"];
-    var redScore = teamReplicant.value[comingUpReplicant.value["next_game"]]["red"]["info"]["tag"] + " - " + teamReplicant.value[comingUpReplicant.value["next_game"]]["red"]["info"]["score"];
-    var updateScore = function(team, newScore) {
-        var scoreTimeline = new TimelineLite();
-        scoreTimeline
-            .to($score[team][0], 0.5, {
-                top: "-110%"
-            })
-            .call(function() {
-                $score[team][1].text(newScore)
-            })
-            .to($score[team][0], 0.5, {
-                top: "0%"
-            });
+    var $score = {"blue": [$("#blue-score"), $("#blue-score-text")], "red": [$("#red-score"), $("#red-score-text")]};
+    var $inGameTexts = {
+        "left": {
+            "top": $("#bottom-center-left-text-upper"),
+            "bottom": $("#bottom-center-left-text-lower")
+        }, "right": $("#bottom-center-right-text")
     };
-    if ($score["blue"][1].text() != blueScore) {
-        updateScore("blue", blueScore)
-    }
-    if ($score["red"][1].text() != redScore) {
-        updateScore("red", redScore)
-    }
-};
 
-var teamReplicant = nodecg.Replicant("team-data")
-    .on("change", function(oldVal, newVal) { // On change...
-        updateScore()
-    });
+    var updateScore = function () {
+        if (!comingUpReplicant.value) { // If the coming up replicant hasn't loaded yet, return.
+            return
+        }
+        var blueScore = teamReplicant.value[comingUpReplicant.value["next_game"]]["blue"]["info"]["score"] + " - " + teamReplicant.value[comingUpReplicant.value["next_game"]]["blue"]["info"]["tag"];
+        var redScore = teamReplicant.value[comingUpReplicant.value["next_game"]]["red"]["info"]["tag"] + " - " + teamReplicant.value[comingUpReplicant.value["next_game"]]["red"]["info"]["score"];
+        var updateScore = function (team, newScore) {
+            var scoreTimeline = new TimelineLite();
+            scoreTimeline
+                .to($score[team][0], 0.5, {
+                    top: "-110%"
+                })
+                .call(function () {
+                    $score[team][1].text(newScore)
+                })
+                .to($score[team][0], 0.5, {
+                    top: "0%"
+                });
+        };
+        if ($score["blue"][1].text() != blueScore) {
+            updateScore("blue", blueScore)
+        }
+        if ($score["red"][1].text() != redScore) {
+            updateScore("red", redScore)
+        }
+    };
 
-var comingUpReplicant = nodecg.Replicant("coming-up")
-    .on("change", function(oldVal, newVal) { // On change...
-        updateScore()
-    });
-
-var inGameReplicant = nodecg.Replicant("in-game")
-    .on("change", function(oldVal, newVal) {
-        $inGameTexts["left"]["top"].text(newVal["left"]["top"]);
-        $inGameTexts["left"]["bottom"].text(newVal["left"]["bottom"]);
-        $inGameTexts["right"].text(newVal["right"]);
-    });
-
-var $scoreBoard = $("#score");
-var $bottomLeft = $("#bottom-left");
-var $bottomCenter = $("#bottom-center");
-
-nodecg.listenFor("lolEvent", function(key) {
-    if (key == "A") { // If teamfight.
-        ($scoreBoard.is(":visible")) ? $scoreBoard.hide() : $scoreBoard.show(); // Toggle score.
-        ($bottomLeft.is(":visible")) ? $bottomLeft.hide() : $bottomLeft.show(); // Toggle info box.
-        ($bottomCenter.is(":visible")) ? $bottomCenter.hide() : $bottomCenter.show(); // Toggle text bar.
-    }
-});
-
-function cycleImages() {
-    var $current = $("#bottom-left-images .active");
-    var $next = ($current.next().length > 0) ? $current.next() : $("#bottom-left-images img:first");
-    $current.fadeOut(1000, function() {
-        $current.removeClass("active"); // Reset z-index and unhide.
-        $next.fadeIn(1000, function() {
-            $next.addClass("active"); // Show next image
+    var teamReplicant = nodecg.Replicant("team-data")
+        .on("change", function (oldVal, newVal) { // On change...
+            updateScore()
         });
-    })
-}
 
-$(document).ready(function() {
+    var comingUpReplicant = nodecg.Replicant("coming-up")
+        .on("change", function (oldVal, newVal) { // On change...
+            updateScore()
+        });
+
+    var inGameReplicant = nodecg.Replicant("in-game")
+        .on("change", function (oldVal, newVal) {
+            $inGameTexts["left"]["top"].text(newVal["left"]["top"]);
+            $inGameTexts["left"]["bottom"].text(newVal["left"]["bottom"]);
+            $inGameTexts["right"].text(newVal["right"]);
+        });
+
+    var $scoreBoard = $("#score");
+    var $bottomLeft = $("#bottom-left");
+    var $bottomCenter = $("#bottom-center");
+
+    nodecg.listenFor("lolEvent", function (key) {
+        if (key == "A") { // If teamfight.
+            ($scoreBoard.is(":visible")) ? $scoreBoard.hide() : $scoreBoard.show(); // Toggle score.
+            ($bottomLeft.is(":visible")) ? $bottomLeft.hide() : $bottomLeft.show(); // Toggle info box.
+            ($bottomCenter.is(":visible")) ? $bottomCenter.hide() : $bottomCenter.show(); // Toggle text bar.
+        }
+    });
+
+    function cycleImages() {
+        var $current = $("#bottom-left-images .active");
+        var $next = ($current.next().length > 0) ? $current.next() : $("#bottom-left-images img:first");
+        $current.fadeOut(1000, function () {
+            $current.removeClass("active"); // Reset z-index and unhide.
+            $next.fadeIn(1000, function () {
+                $next.addClass("active"); // Show next image
+            });
+        })
+    }
+
     setInterval("cycleImages()", 8000);
 });
 
