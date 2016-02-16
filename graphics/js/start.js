392adda1efb7a355b588532aeb518adb519c14b5
Chris Stevens <chris27stevens@gmail.com>
2016-02-16 21:43:29 +0000

    Different coloured start screen, get rid of animations until I can figure out what's wrong with fullscreen video.

--- graphics/js/start.js
+++ graphics/js/start.js
@@ -1,3 +1,31 @@
+/** PRELOAD.JS **/
+var preload = new createjs.LoadQueue(false);
+preload.loadManifest([ // Preload videos.
+    {id: "blue-poster", src: "images/blue.jpg"},
+    {id: "green-poster", src: "images/green.jpg"},
+    {id: "red-poster", src: "images/red.jpg"},
+    {id: "black-poster", src: "images/black.jpg"}
+]);
+preload.load();
+
+preload.on("complete", function() { // Don't do anything until we've loaded assets.
+    var $backgroundVid = $("#background-vid");
+    var $backgroundVidSource = $("#background-vid source");
+    var currentBG = "";
+
+    var comingUpReplicant = nodecg.Replicant("coming-up")
+        .on("change", function(oldVal, newVal) { // On change...
+            var colour = (newVal["colour"] == "inhouse") ? "black" : newVal["colour"];
+            if (currentBG != colour) {
+                $("#background-img").css("background-image", "url(" + preload.getResult(colour + "-poster").src + ")");
+                // $backgroundVid.attr("poster", preload.getResult(newVal["colour"] + "-poster").src);
+                // $backgroundVidSource.attr("src", preload.getResult(newVal["colour"] + "-vid").src); // Actual video.
+                // $backgroundVid.load(); // Load it.
+                currentBG = newVal["colour"];
+            }
+        });
+});
+
 var ready = {
     "start": true,
     "coming-up": false,
