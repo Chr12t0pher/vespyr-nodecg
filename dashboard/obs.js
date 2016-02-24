0dc8f3b778fadeb3390e26e9e62801623566e845
Chris Stevens <chris27stevens@gmail.com>
2016-02-24 23:26:41 +0000

    Make (a heavily 'customised') OBS Remote work on Chrome

--- dashboard/obs.js
+++ dashboard/obs.js
@@ -11,25 +11,17 @@ var $sceneInputs = {
 
 
 /** OBS **/
-var obs = new OBSRemote();
-
-var connect = function(ip) {
-    console.log("[OBS] Connecting on " + ip);
-    obs.connect(ip, "admin");
-};
 
 // Register all the event handlers for OBSRemote.
-obs.onConnectionOpened = function() {
+nodecg.listenFor("obs-onConnectionOpened", function() {
     $obsStatus.removeClass().addClass("alert alert-warning").text("CONNECTING");
-};
+});
 
-obs.onAuthenticationSucceeded = function() {
-    obs.getCurrentScene(function(scene) {
-        $sceneInputs[scene["name"]].prop("checked", true);
-        obsScene.value = scene["name"];
-    });
+nodecg.listenFor("obs-onAuthenticationSucceeded", function() {
+	nodecg.sendMessage("obs-getCurrentScene");
     $obsStatus.removeClass().addClass("alert alert-success").text("CONNECTED");
-    obs.getStreamingStatus(function(stream, previewing) {
+    nodecg.sendMessage("obs-getStreamStatus", function(callback) {
+		var stream = callback.stream; var previewing = callback.previewing;
         if (stream && !previewing) {
             $streamStatus.removeClass().addClass("alert alert-success").text("STREAM LIVE");
             $obsStartPromptBtn.parent().hide();
@@ -41,24 +33,27 @@ obs.onAuthenticationSucceeded = function() {
             $obsStopBtn.parent().hide();
         }
     });
-};
+});
 
-obs.onAuthenticationFailed = function() {
+nodecg.listenFor("obs-currentScene", function(scene) {
+	$sceneInputs[scene["name"]].prop("checked", true);
+	obsScene.value = scene["name"];
+});
+
+nodecg.listenFor("obs-onAuthenticationFailed", function() {
     $obsStatus.removeClass().addClass("alert alert-danger").text("AUTHENTICATION FAILED");
-};
+});
 
-obs.onConnectionFailed = function() {
+nodecg.listenFor("obs-onConnectionFailed", function() {
     $obsStatus.removeClass().addClass("alert alert-danger").text("CONNECTION FAILED");
-    setTimeout(connect(), 5000);
-};
+});
 
-obs.onConnectionClosed = function() {
+nodecg.listenFor("obs-onConnectionClosed", function() {
     $obsStatus.removeClass().addClass("alert alert-danger").text("CONNECTION CLOSED");
-    setTimeout(connect(), 5000);
-};
+});
 
-obs.onStreamStarted = function(previewing) {
-    if (!previewing) {
+nodecg.listenFor("obs-onStreamStarted", function(status) {
+    if (status == "live") {
         $streamStatus.removeClass().addClass("alert alert-success").text("STREAM LIVE");
         $obsStartPromptBtn.parent().hide();
         $obsStopBtn.parent().show();
@@ -67,17 +62,13 @@ obs.onStreamStarted = function(previewing) {
         $obsStartPromptBtn.parent().show();
         $obsStopBtn.parent().hide();
     }
-};
+});
 
-obs.onStreamStopped = function() {
+nodecg.listenFor("obs-onStreamStopped", function() {
     $streamStatus.removeClass().addClass("alert alert-danger").text("STREAM OFFLINE");
     $obsStartPromptBtn.parent().show();
     $obsStopBtn.parent().hide();
-};
-
-obs.onSceneSwitched = function(sceneName) { // If scene is changed in OBS...
-    obsScene.value = {"scene": sceneName, "update": "obs"}; // Update the OBS replicant.
-};
+});
 
 
 /** REPLICANTS **/
@@ -85,8 +76,6 @@ var obsScene = nodecg.Replicant("obs-scene", {defaultValue: {"scene": "", "updat
     .on("change", function(oldVal, newVal) { // On change...
         if (newVal["update"] == "obs") {
             $sceneInputs[newVal["scene"]].prop("checked", true); // Update dashboard.
-        } else if (newVal["update"] == "nodecg") {
-            obs.setCurrentScene(newVal["scene"]); // Update OBS
         }
     });
 
@@ -97,39 +86,10 @@ $.each($sceneInputs, function(scene, value) {
 });
 
 $("#obs-ip-btn").click(function() {
-    if(navigator.userAgent.toLowerCase().indexOf('chrome') > -1) {
-       alert("OBS control to remote hosts does not work in Chrome - please use Firefox or Internet Explorer/Edge." +
-           " If you're connecting to 127.0.0.1 or localhost disregard this message.")
-    }
-    connect($("#obs-ip").val());
+    nodecg.sendMessage("obs-connect", ($("#obs-ip").val()));
 });
 
 /** START/STOP **/
-nodecg.listenFor("obs-start", function() {
-    obs.getStreamingStatus(function(live, preview) {
-        if (live && preview) { // If the preview is live...
-            obs.toggleStream(); // Toggle it off.
-        }
-    });
-    setTimeout(function() {
-        obs.toggleStream(); // Go live!
-        obs.setCurrentScene("start"); // Load the start scene first, so we can listen for the loaded messages.
-        setTimeout(function() {
-            obs.setCurrentScene("coming-up"); // Go through all the scenes to load the web-pages.
-            obs.setCurrentScene("champ-select");
-            obs.setCurrentScene("loading");
-            obs.setCurrentScene("in-game");
-            obs.setCurrentScene("analysis");
-            obs.setCurrentScene("end");
-            obs.setCurrentScene("start"); // End on the start screen deliberately.
-        }, 1000)
-    }, 1000); // Go live after 1 second to account for turning off the preview.
-});
-
 $obsStopBtn.click(function() {
-    obs.getStreamingStatus(function(live, preview) {
-        if (live) {
-            obs.toggleStream();
-        }
-    });
+    nodecg.sendMessage("obs-stop");
 });
--- extension/index.js
+++ extension/index.js
@@ -3,4 +3,5 @@
 module.exports = function(nodecg) {
     require("./keypress.js")(nodecg);
     require("./strawpoll.js")(nodecg);
+	require("./obs.js")(nodecg);
 };
